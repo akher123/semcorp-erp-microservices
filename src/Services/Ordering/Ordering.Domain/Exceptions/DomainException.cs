215c513718d0ff6701870f4d81592f63dfa65f67
akheruzzaman <akher.ice07@gmail.com>
2025-12-09 01:26:26 +0600

    Feature: Develop Ordering domain layer with Enity, Aggregate, Aggregate Roots, Value Object and Models etc

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Exceptions;
+
+public class DomainException:Exception
+{
+    public DomainException()
+    {
+    }
+    public DomainException(string message) : base($"Domain Exception:\"{message}\" Throws Domain Layer.")
+    {
+    }
+ 
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace Ordering.Domain.Models;
+
+public class Order:Aggregate<OrderId>
+{
+    private  readonly List<OrderItem> _orderItems=new();
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly(); 
+   
+    public CustomerId CustomerId { get; private set; }=default!;
+    public OrderName OrderName { get; private set; }=default!;
+
+    public Address ShippingAddress { get; private set;} = default!;
+
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; }=default!;
+
+    public OrderStatus Status { get; private set; }=OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get=> _orderItems.Sum(oi => oi.Price * oi.Quantity);
+        private set { }
+    }
+
+    public static Order Create(OrderId id,  CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, List<OrderItem> orderItems)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+    {
+        var order = new Order
+        {
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = status,
+        };
+        AddDomainEvent(new OrderUpdatedEvent(order));
+    }
+}
