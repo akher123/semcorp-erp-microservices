3cfe64f5b4d051946c821fe0fdefe188750954df
akheruzzaman <akher.ice07@gmail.com>
2025-12-08 20:10:33 +0600

    feat: add BuildingBlocks class library for CQRS and cross-cutting behaviors
    - Added pipeline behaviors (validation, logging)

---
+++ src/BuildingBlocks/BuildingBlock/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlock.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlock.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>
+ (IEnumerable<IValidator<TRequest>> validators)
+ : IPipelineBehavior<TRequest, TResponse>
+ where TRequest : ICommand<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+        var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var featurs =
+            validationResults
+            .Where(r => r.Errors.Any())
+            .SelectMany(r => r.Errors)
+            .ToList();
+        if (featurs.Any())
+            throw new ValidationException(featurs);
+
+        return await next();
+
+    }
+}
--- src/BuildingBlocks/BuildingBlock/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlock/CQRS/IQueryHandler.cs
@@ -1,8 +1,4 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace BuildingBlock.CQRS;
 public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
     where TQuery : IQuery<TResponse>
